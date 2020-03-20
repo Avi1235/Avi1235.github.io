2991590938894656098ea6766b386ad13ba0ee25
david <david-prz98@hotmail.com>
2020-03-20 14:30:38 -0700

    await 3

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
     xobj.open('GET', 'servicios.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function async () {
+    xobj.onreadystatechange = async function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             await callback(xobj.responseText);
