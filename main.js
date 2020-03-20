2c49e8118ba52442b4b2104cd043d6477cc4885c
david <david-prz98@hotmail.com>
2020-03-20 14:28:01 -0700

    await 2

--- main.js
+++ main.js
@@ -5,27 +5,30 @@ function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
     xobj.open('GET', 'servicios.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
+    xobj.onreadystatechange = function async () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
+            await callback(xobj.responseText);
           }
     };
     xobj.send(null);  
  }
 
- async function init() {
+function init() {
     loadJSON(function(response) {
      // Parse JSON string into object
-     objetoJSON = await JSON.parse(response);
+     objetoJSON = JSON.parse(response);
+     return objetoJSON;
     });
 }
 
 function buscarServicio() {
   var numero = document.getElementById('servicio').value;
-  init();
+  var data = init();
+  if(data==null)
+  alert("No se encuentra el servicio,Intentelo de nuevo");
   var encontro = false;
-  objetoJSON.forEach(i => {
+  data.forEach(i => {
     if(i.servicio==numero)
     {
       const element = i;
