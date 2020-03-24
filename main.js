2539d03a5d44bd961318e59182847526ac5f9edc
david <david-prz98@hotmail.com>
2020-03-24 03:04:46 -0700

    sumas2

--- main.js
+++ main.js
@@ -65,10 +65,10 @@ function abonar() {
 }
     
 function sumar(cantidad){
-  var suma = document.getElementById('suma').value;
-  suma += cantidad;
+  var suma = parseInt(document.getElementById('suma').value);
+  suma += parseInt(cantidad);
   document.getElementById('suma').value = suma;
-  document.getElementById('suma').innerHTML = suma;
+  document.getElementById('suma').innerHTML = suma.toString();
 }
 
 function abrirmodal(){
